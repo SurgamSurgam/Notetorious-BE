53c36e2c8c9b1154b6d694f82fe5a372d3ae262d
Deyvi <deyvio@gmail.com>
2019-03-30 18:46:05 -0400

    fixing git issues

--- frontend/src/actions/FavoritesActions.js
+++
@@ -1,5 +0,0 @@
-import { RECEIVE_ID_FOR_SELECTED_NOTE_FROM_FAVORITES } from "./actionTypes";
-
-export const receiveIdForSelectedNoteFromFavorites = id => {
-  return { type: RECEIVE_ID_FOR_SELECTED_NOTE_FROM_FAVORITES, id };
-};
