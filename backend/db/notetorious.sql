f24e20bf90f699e548fc36c7a83eed8261cf3c45
Deyvi <deyvio@gmail.com>
2019-03-30 17:07:48 -0400

    set up for heroku + added env.DB to be used on backend connections

--- backend/db/notetorious.sql
+++ backend/db/notetorious.sql
@@ -1,7 +1,8 @@
-DROP DATABASE IF EXISTS notetorious;
-CREATE DATABASE notetorious;
-
-\c notetorious;
+DROP TABLE  IF EXISTS taggings;
+DROP TABLE IF EXISTS tags ;
+DROP TABLE  IF EXISTS notes;
+DROP TABLE  IF EXISTS notebooks;
+DROP TABLE  IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -41,3 +42,13 @@ CREATE TABLE taggings (
   note_id INT REFERENCES notes(id),
   tag_id INT REFERENCES tags(id) ON DELETE CASCADE
 );
+
+INSERT INTO users(username, email, password_digest, profile_pic) VALUES ('Clark Kent', 'kent@super.com', '$2a$10$XhmVGRJ6y26q6GGP7Dcicudow2FygRu.j0T.sD4N2B2BzGwjHLwlu', 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-Y5ZwCqAT6m0%2FTzVcYOE0X4I%2FAAAAAAAAI30%2FsQJFzBftSxM%2Fs400%2Fclark-kent-and-superman.jpg&f=1');
+
+INSERT INTO notebooks(author_id, title, is_default) VALUES (1, 'Metropolis', TRUE);
+
+INSERT INTO notes(title, body, author_id, notebook_id, favorited) VALUES ('Daily Planet', 'Clark Joseph Kent is a fictional character appearing in American comic books published by DC Comics. Created by Jerry Siegel and Joe Shuster, he debuted in Action Comics #1 (June 1938) and serves as the civilian and secret identity of the superhero Superman.', 1, 1, TRUE);
+
+INSERT INTO tags(name) VALUES ('superhero');
+
+INSERT INTO taggings(note_id, tag_id) VALUES (1, 1);
